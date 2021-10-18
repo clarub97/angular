9ee46427221336a56ca7df6d89bb254799a586e5
clarub97 <claudio.rubino@binarydigit.it>
2021-10-18 16:55:54 +0000

    loginfinito

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,11 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', vai: 'Cliccami', link: '/test' });
+  res.render('index', { title: 'Express', vai: 'Cliccami', link: '/login' });
 });
-/* GET test page. */
-router.get('/test', function(req, res, next) {
-  res.render('test', { title: 'test'});
+/* GET Login */
+router.get('/login', function(req, res, next) {
+  res.render('login', {title: 'Login'});
+});
+/* GET Register page */
+router.get('/registrazione', function(req, res, next) {
+  res.render('registrazione', {title: 'Registrati'});
+});
+/* GET account's informations */
+router.get('/account', function(req, res, next) {
+  res.render('account', {title: 'Account'});
 });
 
 module.exports = router;
